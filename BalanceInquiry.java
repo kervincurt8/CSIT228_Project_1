e45782956a761e6994bb2105ba66e5666608ee7f
Kervin Curt Delos Reyes <curtdelosreyes8@gmail.com>
2026-03-05 21:32:37 +0800

    Back to initial structure

--- BalanceInquiry.java
+++
@@ -1,7 +0,0 @@
-public class BalanceInquiry {
-
-    public void showBalance(Account account){
-        System.out.println("\n===== BALANCE INQUIRY =====");
-        account.displayAccount();
-    }
-}
